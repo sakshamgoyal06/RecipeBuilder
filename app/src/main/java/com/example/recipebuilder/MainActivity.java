e7a516046a1fb6e7634f6a704e616b0da7e47896
sakshamgoyal06 <sakshamgoyal06@gmail.com>
2020-05-16 02:14:20 +0530

    Add activity UI Basic

--- app/src/main/java/com/example/recipebuilder/MainActivity.java
+++ app/src/main/java/com/example/recipebuilder/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.recipebuilder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+private BottomNavigationView botbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botbar = findViewById(R.id.home_bot);
+        botbar.setOnNavigationItemSelectedListener(navlistner);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if(item.getItemId()==R.id.add_home){
+                        Intent addRecipe = new Intent(MainActivity.this,addrecipe.class);
+                        startActivity(addRecipe);
+                    }
+
+                    return true;
+                }
+            };
 }
